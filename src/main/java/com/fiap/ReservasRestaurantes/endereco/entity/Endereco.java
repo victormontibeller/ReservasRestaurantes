03d4afe43ee96225d93f3d3a75c0f4727e47ea62
alexmo3 <alexandre.marq@gmail.com>
2024-02-16 11:05:55 -0300

    Limpeza de imports sem uso

--- src/main/java/com/fiap/ReservasRestaurantes/endereco/entity/Endereco.java
+++ src/main/java/com/fiap/ReservasRestaurantes/endereco/entity/Endereco.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
